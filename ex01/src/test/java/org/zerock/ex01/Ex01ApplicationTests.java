0e3b7cccf720923c1e984e7c208d7cef4af85cc8
ttyui0402 <ttyui0402@gmail.com>
2021-01-17 15:23:20 +0900

    Springboot part1/chap01/ex01

---
+++ ex01/src/test/java/org/zerock/ex01/Ex01ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("contextLoads..........");
+    }
+
+}
