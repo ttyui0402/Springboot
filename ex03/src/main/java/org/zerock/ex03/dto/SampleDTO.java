222f4e9132245028480ddf9039ed002fc1c7ea34
ttyui0402 <ttyui0402@gmail.com>
2021-01-17 19:50:23 +0900

    add Spring boot part 1 / chap03 /  ex03

---
+++ ex03/src/main/java/org/zerock/ex03/dto/SampleDTO.java
@@ -0,0 +1,20 @@
+package org.zerock.ex03.dto;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
