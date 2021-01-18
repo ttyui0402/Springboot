7b18b4ba64300819c1b597283f0226d592773a57
ttyui0402 <ttyui0402@gmail.com>
2021-01-18 20:47:58 +0900

    modify GuestbookController

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookContorller.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookContorller.java
@@ -86,8 +86,8 @@ public class GuestbookContorller {
 
         service.modify(dto);
 
-        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
-        redirectAttributes.addFlashAttribute("gno", dto.getGno());
+        redirectAttributes.addAttribute("page", requestDTO.getPage());
+        redirectAttributes.addAttribute("gno", dto.getGno());
 
         return "redirect:/guestbook/read";
     }
