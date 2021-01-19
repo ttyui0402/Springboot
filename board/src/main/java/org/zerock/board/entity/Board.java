ee4cde38c9545aa56f9666900a3e60be39d31a01
ttyui0402 <ttyui0402@gmail.com>
2021-01-19 19:02:23 +0900

    add Springboot part 3 / board

---
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -0,0 +1,23 @@
+package org.zerock.board.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer")   // @ToString은 항상 exclude
+public class Board extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member writer;  //  연관관계 지정
+}
---
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -0,0 +1,41 @@
+package org.zerock.board.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Member;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void insertBoard() {
+        IntStream.rangeClosed(1,100).forEach(i -> {
+
+            Member member = Member.builder().email("user" + i + "@aaa.com").build();
+
+            Board board = Board.builder().title("Title....." + i).content("Content....." + i).writer(member).build();
+
+            boardRepository.save(board);
+        });
+    }
+
+    @Transactional
+    @Test
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
+    }
+}
